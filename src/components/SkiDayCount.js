9ce6b46b3b04d51f08d93a97fac8a2fe5058bf8f
Julian Craigwell-Graham <jccraigw@gmail.com>
2017-10-04 20:43:24 -0500

    react icons

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 export class SkiDayCount extends Component {
+  percentToDecimal(decimal){
+
+    return ((decimal * 100) + '%')
+  }
+  calcGoalProgress(total, goal){
+
+    return this.percentToDecimal(total/goal)
+  }
   render() {
     return (
       <div className="ski-day-count">
         <div className="total-day">
           <span>{this.props.total}</span>
+          <Calendar />
           <span> days</span>
         </div>
         <div className="powder-days">
           <span>{this.props.powder}</span>
+          <SnowFlake />
           <span> days</span>
         </div>
         <div className="backcountry-days">
           <span>{this.props.backcountry}</span>
-          <span> hiking day</span>
+           <Terrain />
+          <span> day</span>
         </div>
         <div>
-          <span>{this.props.goal}</span>
+          <span>
+            {this.calcGoalProgress(
+                this.props.total,
+                this.props.goal
+                )}
+          </span>
         </div>
       </div>
     );
